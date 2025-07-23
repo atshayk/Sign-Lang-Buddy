73e68d303d0ae2005330b156644935d6916c0a10
atshayk <anistimurshayk@gmail.com>
2025-07-23 14:11:11 +0530

    fixed a whole internal mess of modules, now seems to be stable

---
+++ test_imports.py
@@ -0,0 +1,11 @@
+import cv2
+import mediapipe
+import sklearn
+import imutils
+import flask
+import flask_socketio
+import flask_cors
+import tensorflow
+
+
+print("✅ All modules imported successfully!")
